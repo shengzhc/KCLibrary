6283e13e1115dd01fa4e24251290c0788d7aa265
shengzhc <shengzhc@usc.edu>
2013-06-22 14:51:37 -0700

    Init KChen

---
+++ KCLibrary/KCLibrary/KCLibrary.h
@@ -0,0 +1,13 @@
+//
+//  KCLibrary.h
+//  KCLibrary
+//
+//  Created by Shengzhe Chen on 6/22/13.
+//  Copyright (c) 2013 Shengzhe Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KCLibrary : NSObject
+
+@end
---
+++ KCLibrary/KCLibrary/KCLibrary.m
@@ -0,0 +1,13 @@
+//
+//  KCLibrary.m
+//  KCLibrary
+//
+//  Created by Shengzhe Chen on 6/22/13.
+//  Copyright (c) 2013 Shengzhe Chen. All rights reserved.
+//
+
+#import "KCLibrary.h"
+
+@implementation KCLibrary
+
+@end
