1c593e743a724886812ab52ec09a21449b348ae4
shengzhc <shengzhc@usc.edu>
2013-07-14 17:47:48 -0700

    set up kclibrary test

---
+++ KCLibraryTest/KCLibraryTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KCLibraryTest
+//
+//  Created by Shengzhe Chen on 7/14/13.
+//  Copyright (c) 2013 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KCLibraryTest/KCLibraryTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KCLibraryTest
+//
+//  Created by Shengzhe Chen on 7/14/13.
+//  Copyright (c) 2013 Shengzhe Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
